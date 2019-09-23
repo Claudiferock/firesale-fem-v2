1b8110c4be05096b0436032f17ec1156abb3e03a
Claudio Rodríguez Vásquez <claudifero@gmail.com>
2019-09-23 20:58:09 +0300

    Add BrowserWindow

--- app/main.js
+++ app/main.js
@@ -1,1 +1,14 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow } = require('electron');   // switched electron for app as its more lazy way to go about
+
+// Our BrowserWindow is defined here so it wont get garbage collected for being inside the event listener.
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow();
+
+    mainWindow.loadFile(`${__dirname}/index.html`);
+    console.timeEnd("Time taken to be ready: ");
+});
+
+console.log('Starting up...');
+console.time("Time taken to be ready: ");
