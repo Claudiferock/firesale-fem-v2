4293cc776605a200c3ce00644f94cfc6600ad149
Claudio <claudifero@gmail.com>
2019-09-24 11:50:59 +0300

    Add openFile
    Open file (text file or markdown) from user and show it as a string

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron');   // switched electron for app as its more lazy way to go about
+const fs = require('fs');
+
+const { app, BrowserWindow, dialog } = require('electron');   // switched electron for app as its more lazy way to go about
 
 // Our BrowserWindow is defined here so it wont get garbage collected for being inside the event listener.
 let mainWindow = null;
@@ -8,6 +10,8 @@ app.on('ready', () => {
 
     mainWindow.loadFile(`${__dirname}/index.html`);
 
+    getFileFromUser();
+
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
         console.timeEnd("Time taken to be ready: ");
@@ -16,3 +20,20 @@ app.on('ready', () => {
 
 console.log('Starting up...');
 console.time("Time taken to be ready: ");
+
+const getFileFromUser = () => {
+    const files = dialog.showOpenDialog({
+        properties: ['openFile'],
+        filters: [
+            { name: 'Markdown Files', extensions: ['md', 'mdown', 'markdown', 'marcdown'] },
+            { name: 'Text Files', extensions: ['txt', 'text'] }
+        ]
+    });
+
+    if (!files) return; // this way we avoid undefined if user clicks calcel
+
+    const file = files[0];
+    const content = fs.readFileSync(file).toString();
+
+    console.log(content);
+};
